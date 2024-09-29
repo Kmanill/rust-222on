8cc779d6aaeba7d3dc9aa46638d498d0f3629544
Kmanill <v.danyuk@e-u.edu.ua>
2024-09-30 00:37:58 +0300

    commit:rust by practice 3-10 Vlad Danyuk

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+mod variables3;
+mod basictypes4;
+mod ownershipandborrowing5;
+mod compound_types_string;
+mod compound_types_array;
+mod compound_types_slice;
+mod compound_types_tuple;
+mod compound_types_struct;
+mod compound_types_enum;
+mod flow_control;
+mod pattern_match;
+mod associated_functions_methods;
+mod generics;
+mod const_generics;
+mod traits;
+mod trait_object;
+mod advance_traits;
+
+fn main() {
+    println!("Hello, world!");
+}
